853b5e0bd3d074ee4ab87d57dc1a871c00bc93ce
mansoura50 <adammansour600@gmail.com>
2019-05-01 23:27:43 -0400

    Initial commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    //bullet damage
+    public int damage = 5;
+
+    // Start is called before the first frame update
+    void Start() {
+        //shots bullet right 
+        rb.velocity = transform.right * speed;
+    }
+
+    //causes bullets to damage from HurtEnemy and EnemyHealthManager scripts
+    void OnTriggerEnter2D(Collider2D hitInfo) {
+        EnemyHealthManager enemy = hitInfo.GetComponent<EnemyHealthManager>();
+        if (enemy != null) {
+            enemy.HurtEnemy(damage);
+        }
+        Destroy(gameObject);    
+    }
+
+}
